670a9406bd797e8f145b78fb651c3d3c96c21945
Paulo1402 <paulobenatto02@gmail.com>
2023-09-29 18:58:45 -0300

    create theme toggle

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,13 +1,18 @@
 <template>
   <v-app-bar flat>
-    <v-app-bar-title>
-      <v-icon icon="mdi-circle-slice-4" />
-
-      Base Preset
-    </v-app-bar-title>
+    <v-app-bar-title class="">Pruvo Challenge</v-app-bar-title>
+    <v-btn class="flex-grow-0" prepend-icon="mdi-circle-slice-4" @click="toggleTheme">
+      Mudar tema
+    </v-btn>
   </v-app-bar>
 </template>
 
 <script lang="ts" setup>
-  //
+import { useTheme } from "vuetify";
+
+const theme = useTheme();
+
+function toggleTheme() {
+  theme.global.name.value = theme.global.current.value.dark ? "light" : "dark";
+}
 </script>
