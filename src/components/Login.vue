192df5e9378c0ad1625837b85c91b40c0ff7b379
Paulo1402 <paulobenatto02@gmail.com>
2023-09-30 20:22:34 -0300

    create login/signup form

---
+++ src/components/Login.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container>
+    <v-card class="mx-auto mt-4 px-6 py-8" max-width="400">
+      <v-card-title class="pb-4">LOGIN</v-card-title>
+
+      <v-form v-model="form" @submit.prevent="handleSubmit">
+        <v-text-field
+          class="mb-2"
+          v-model="email"
+          label="E-mail"
+          clearable
+          type="email"
+          :rules="[validateEmail]"
+        ></v-text-field>
+        
+        <v-text-field
+          class="mb-2"
+          v-model="password"
+          label="Senha"
+          :type="showPassword ? 'text' : 'password'"
+          :append-inner-icon="showPassword ? 'mdi-eye-off' : 'mdi-eye'"
+          @click:append-inner="showPassword = !showPassword"
+          :rules="[validatePassword]"
+        ></v-text-field>
+
+        <v-btn
+          class="mt-4"
+          block
+          variant="elevated"
+          size="large"
+          color="success"
+          type="submit"
+          :disabled="!form"
+          >Entrar</v-btn
+        >
+      </v-form>
+
+      <v-card-text class="text-center"
+        ><v-btn variant="plain" :ripple="false" @click="$emit('signUp')"
+          >Cadastrar agora</v-btn
+        ></v-card-text
+      >
+    </v-card>
+  </v-container>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import router from "@/router";
+
+import { signIn } from "@/services/firebase";
+
+const email = ref("");
+const password = ref("");
+const showPassword = ref(false);
+const form = ref(false);
+
+async function handleSubmit() {
+  const user = await signIn(email.value, password.value);
+
+  if (user) {
+    await router.push("/");
+    return;
+  }
+
+  alert("Usuário inválido");
+}
+
+function validatePassword(password: string) {
+  return password.length >= 6 || "Mínimo 6 caracteres.";
+}
+
+function validateEmail(email: string) {
+  const pattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(email) || "E-mail inválido.";
+}
+</script>
---
+++ src/views/Login.vue
@@ -0,0 +1,18 @@
+<template>
+  <Login v-if="showSignUp" @sign-up="toggleForm"/>
+  <SignUp v-else @login="toggleForm"/>
+
+</template>
+
+<script setup lang="ts">
+  import { ref } from "vue";
+
+  import SignUp from "@/components/SignUp.vue";
+  import Login from "@/components/Login.vue";
+
+  const showSignUp = ref(false)
+
+  function toggleForm() {
+    showSignUp.value = !showSignUp.value
+  }
+</script>
