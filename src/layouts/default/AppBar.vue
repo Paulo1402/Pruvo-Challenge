b257f176b36738f5ddc188ee01003e7f003c1ab9
Paulo1402 <paulobenatto02@gmail.com>
2023-10-01 16:26:36 -0300

    tests in appbar layout

--- src/layouts/default/AppBar.vue
+++ src/layouts/default/AppBar.vue
@@ -1,25 +1,27 @@
 <template>
-  <v-navigation-drawer v-model="isDrawerOpen">
+  <v-navigation-drawer v-if="isAuthenticated" v-model="isDrawerOpen">
     <v-list>
-      <v-list-subheader> Menu </v-list-subheader>
+      <v-list-subheader>Menu</v-list-subheader>
       <v-list-item prepend-icon="mdi-home">Home</v-list-item>
+      <v-list-item prepend-icon="mdi-home">Provas</v-list-item>
+      <v-list-item prepend-icon="mdi-home">Nova Prova</v-list-item>
     </v-list>
   </v-navigation-drawer>
 
-  <v-app-bar flat>
-    <v-app-bar-nav-icon
+  <v-app-bar>
+    <v-app-bar-nav-icon v-if="isAuthenticated"
       @click="isDrawerOpen = !isDrawerOpen"
     ></v-app-bar-nav-icon>
 
     <v-app-bar-title>
-      <v-btn :ripple="false" variant="plain" to="/">Pruvo Challenge </v-btn>
+      <v-btn :ripple="false" variant="plain" to="/">Pruvo Challenge</v-btn>
     </v-app-bar-title>
 
-    <v-btn prepend-icon="mdi-circle-slice-4" @click="toggleTheme">
+    <v-btn class="mr-2" prepend-icon="mdi-circle-slice-4" @click="toggleTheme">
       Mudar tema
     </v-btn>
 
-    <v-btn color="red" variant="tonal">
+    <v-btn v-if="isAuthenticated" color="red" variant="tonal">
       Sair
       <v-dialog v-model="isDialogOpen" activator="parent" width="auto">
         <v-card title="ATENÇÃO">
@@ -27,7 +29,7 @@
 
           <v-card-actions>
             <v-spacer />
-            <v-btn :disabled="loading" :loading="loading" color="red" @click="signOut">Sim</v-btn>
+            <v-btn :disabled="isSignOutLoading" :loading="isSignOutLoading" color="red" @click="signOut">Sim</v-btn>
             <v-btn color="primary" @click="isDialogOpen = false">Não</v-btn>
           </v-card-actions>
         </v-card>
@@ -37,15 +39,16 @@
 </template>
 
 <script lang="ts" setup>
-import { ref } from "vue";
+import { ref} from "vue";
 import { useTheme } from "vuetify";
 
 import { Auth } from "@/services/firebase";
 import router from "@/router";
 
-const loading = ref(false)
+const isSignOutLoading = ref(false)
 const isDialogOpen = ref(false);
 const isDrawerOpen = ref(false);
+const isAuthenticated = ref(true);
 
 const theme = useTheme();
 
@@ -54,11 +57,11 @@ function toggleTheme() {
 }
 
 async function signOut() {
-  loading.value = true
+  isSignOutLoading.value = true
 
   await Auth.signOut();
 
-  loading.value = false
+  isSignOutLoading.value = false
   isDialogOpen.value = false
 
   router.push('/login')
