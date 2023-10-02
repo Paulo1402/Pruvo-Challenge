fad377802502276fea30b9406a3b3828499dff47
Paulo1402 <paulobenatto02@gmail.com>
2023-10-02 20:16:40 -0300

    create validation lib

---
+++ src/libs/validation.ts
@@ -0,0 +1,11 @@
+function validatePassword(password: string) {
+  return password.length >= 6 || "Mínimo 6 caracteres.";
+}
+
+function validateEmail(email: string) {
+  const pattern =
+    /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return pattern.test(email) || "E-mail inválido.";
+}
+
+export { validatePassword, validateEmail };
