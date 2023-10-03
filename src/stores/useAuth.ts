a361779b577b29ac4a067d7e7626cc6491c969cd
Paulo1402 <paulobenatto02@gmail.com>
2023-10-03 19:15:37 -0300

    create useAuth to manage authentication state

---
+++ src/stores/useAuth.ts
@@ -0,0 +1,19 @@
+import { defineStore } from "pinia";
+import { ref } from "vue";
+
+export const useAuthStore = defineStore("auth", () => {
+  const isAuthenticated = ref(false)
+  const authenticatedUserName = ref('')
+
+  function loggin(username: string): void {
+    isAuthenticated.value = true
+    authenticatedUserName.value = username
+  }
+
+  function signOut(): void {
+    isAuthenticated.value = false
+    authenticatedUserName.value = ''
+  }
+
+  return { isAuthenticated, authenticatedUserName, loggin, signOut };
+});
