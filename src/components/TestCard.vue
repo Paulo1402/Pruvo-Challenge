dd1480f088d562c2e59485fc7ff8a51a23062f71
Paulo1402 <paulobenatto02@gmail.com>
2023-10-03 14:46:02 -0300

    add error feedback in the TestCard component

--- src/components/TestCard.vue
+++ src/components/TestCard.vue
@@ -1,28 +1,39 @@
 <template>
-  <v-card width="400" class="mx-auto">
+  <v-card max-width="600" class="mx-auto">
+    <v-alert
+      v-model="showError"
+      closable
+      type="error"
+      text="Algo deu errado durante a interação!"
+    >
+    </v-alert>
+
     <div :class="['bg-color', 'bg-' + color]"></div>
 
-    <v-card-title>{{  name  }}</v-card-title>
-    <v-card-text>Atualizado em: {{  updatedAt  }}</v-card-text>
+    <v-card-title>{{ name }}</v-card-title>
+    <v-card-text>Atualizado em: {{ updatedAt }}</v-card-text>
 
     <v-card-actions>
       <v-spacer></v-spacer>
 
       <v-btn variant="tonal" :to="'test/' + id">Editar</v-btn>
-      <DeleteButton :test-id="id" />
+      <DeleteButton @click="showError = false" @error="showError = true" :test-id="id" />
     </v-card-actions>
   </v-card>
 </template>
 
 <script setup lang="ts">
-  import DeleteButton from '@/components/DeleteButton.vue'
+import DeleteButton from "@/components/DeleteButton.vue";
+import { ref } from "vue";
 
-  defineProps<{id: string, name: string, updatedAt: string, color: string}>()
+defineProps<{ id: string; name: string; updatedAt: string; color: string }>();
+
+const showError = ref(false);
 </script>
 
 <style>
-  .bg-color {
-    width: 100%;
-    height: 50px;
-  }
+.bg-color {
+  width: 100%;
+  height: 50px;
+}
 </style>
