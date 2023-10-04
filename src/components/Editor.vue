fcd542ff87b04f82a66ee7f4bdd84a04e74c996d
Paulo1402 <paulobenatto02@gmail.com>
2023-10-03 21:16:20 -0300

    create editor templates

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -4,26 +4,36 @@
       <Editor
         v-model="localContent"
         :api-key="apiKey"
+        :init="{
+          plugins:
+            'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons accordion',
+        }"
         @init="handleEditorInit"
       />
     </v-col>
 
     <v-col md="3" cols="12">
-      <v-container class="pa-0">
-        <v-btn class="mb-4" color="blue" @click="insertText('Discursiva')" block
-          >Discursiva</v-btn
-        >
+      <v-container class="pa-0 mb-8">
         <v-btn
+          v-for="(question, i) in questionsTemplate"
+          :key="i"
+          variant="tonal"
           class="mb-4"
-          color="orange"
-          @click="insertText('Múltipla Escolha')"
+          color="primary"
+          @click="insertText(question.template)"
           block
-          >Múltipla Escolha</v-btn
-        >
-        <v-btn class="mb-4" color="green" @click="insertText('Matriz')" block
-          >Matriz</v-btn
-        >
+          >{{ question.name }}
+        </v-btn>
       </v-container>
+
+      <v-tooltip location="bottom">
+        <template v-slot:activator="{ props }">
+          <v-btn variant="elevated" icon v-bind="props">
+            <v-icon color="primary"> mdi-help </v-icon>
+          </v-btn>
+        </template>
+        <span>Posicione o cursor no editor e clique em um dos modelos de questões! :)</span>
+      </v-tooltip>
     </v-col>
   </v-row>
 </template>
@@ -31,6 +41,8 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
 import Editor from "@tinymce/tinymce-vue";
+
+import { questionsTemplate } from "@/libs/editor";
 
 const props = defineProps<{ modelValue: string }>();
 const emit = defineEmits(["update:modelValue"]);
---
+++ src/libs/editor.ts
@@ -0,0 +1,18 @@
+const questionsTemplate = [
+  {
+    name: "Discursiva",
+    template:
+      "<p>1 -&nbsp;Qual &eacute; a import&acirc;ncia da Revolu&ccedil;&atilde;o Industrial no desenvolvimento da sociedade moderna?&nbsp;</p><p>&nbsp;</p>",
+  },
+  {
+    name: "Múltipla escolha",
+    template:
+      "<p>2 - Qual &eacute; a capital do Brasil?&nbsp;</p><p>a) Rio de Janeiro</p><p>b)&nbsp;S&atilde;o Paulo</p><p>c)&nbsp;Bras&iacute;lia</p><p>d) Salvador&nbsp;</p><p>&nbsp;</p>",
+  },
+  {
+    name: "Matriz",
+    template: `<p>3 - Complete a&nbsp;tabela a seguir com os valores corretos para a equa&ccedil;&atilde;o <span aria-hidden="true">y=2x+3</span>:</p><table style="border-collapse: collapse; width: 100%; height: 90px; margin-left: auto; margin-right: auto;" border="1"><colgroup><col style="width: 50%;"><col style="width: 50%;"></colgroup><tbody><tr style="height: 18px;"><td style="height: 18px; text-align: center;">x</td><td style="height: 18px; text-align: center;">y</td></tr><tr style="height: 18px;"><td style="height: 18px;">1</td><td style="height: 18px;">&nbsp;</td></tr><tr style="height: 18px;"><td style="height: 18px;">2</td><td style="height: 18px;">&nbsp;</td></tr><tr style="height: 18px;"><td style="height: 18px;">3</td><td style="height: 18px;">&nbsp;</td></tr><tr style="height: 18px;"><td style="height: 18px;">4</td><td style="height: 18px;">&nbsp;</td></tr><tr><td>5</td><td>&nbsp;</td></tr></tbody></table><p>&nbsp;</p>`,
+  },
+];
+
+export { questionsTemplate };
